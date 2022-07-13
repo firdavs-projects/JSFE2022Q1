4d872a35b9e6f0a84c93ade0509a12943dd7f76e
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-07-13 12:23:03 +0500

    feat: add logo & link to RS school

--- online-store/src/components/Footer/index.tsx
+++ online-store/src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Container} from "react-bootstrap";
+import rsLogo from '../../assets/icons/logo_rs_text.svg';
 
 const Footer = () => {
     const getCurrentYear = () => {
@@ -8,9 +9,16 @@ const Footer = () => {
     }
     return (
         <footer className="footer bg-dark">
-            <Container className="py-2">
-                <p className="text-light">&copy; Copyright - {getCurrentYear()}. Rolling Scopes School</p>
-                <p className="text-muted m-0">Developed by <a href="#">Firdavs Abdulloev</a></p>
+            <Container className="py-2 d-flex align-items-center justify-content-between">
+                <a href="https://rs.school/"><img src={rsLogo} width={100} alt="RS School"/></a>
+                <div className="text-end">
+                    <p className="text-light">
+                        &copy; Copyright - {getCurrentYear()}. Rolling Scopes School
+                    </p>
+                    <p className="text-muted m-0">
+                        Developed by <a href="https://github.com/firdavs-projects">Firdavs Abdulloev</a>
+                    </p>
+                </div>
             </Container>
         </footer>
     );
--- online-store/src/components/NavBar/index.tsx
+++ online-store/src/components/NavBar/index.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {Button, Container, Navbar} from "react-bootstrap";
 import {Icon} from "ts-react-feather-icons";
+import rsLogo from '../../assets/icons/logo_rs_text.svg';
 
 export interface NavbarProps {
     title: string;
@@ -13,7 +14,7 @@ const NavBar: FC<NavbarProps> = ({title, count, onClick}) => {
         <header className="position-sticky top-0" style={{zIndex: 10}}>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">Online Store</Navbar.Brand>
+                    <Navbar.Brand href="/"><img src={rsLogo} alt="RS School"/> Store</Navbar.Brand>
                     <Navbar.Text>{title}</Navbar.Text>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
