784ad48ed2d060992b55b4f9657de4ea19d103e3
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-07-21 18:30:34 +0500

    fix: remove comments

--- online-store/src/App.test.tsx
+++ online-store/src/App.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-// is the element on the page
 test('renders author link', () => {
     render(<App/>);
     const linkElement = screen.getByText(/Firdavs Abdulloev/i);
--- online-store/src/components/NavBar/Navbar.test.tsx
+++ online-store/src/components/NavBar/Navbar.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import NavBar from '.';
 
-
-// test navbar tag must include the nav tag
 test('renders needed title', () => {
     const title = 'RS School Store';
     render(<NavBar onClick={()=> {}} count={5} title={title} key={1}/>);
